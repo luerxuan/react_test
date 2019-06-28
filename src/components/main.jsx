f8a6cc6531b8a34fb8039eb9f46d197b6f45b287
baogaijie <1006214269@qq.com>
2019-06-28 14:13:21 +0800

    【路尔轩】点击搜索时请求接口

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,6 +1,12 @@
 import React,{Component} from 'react'
+import PropTypes from 'prop-types'
+import axios from 'axios'
 
 export default class Main extends Component{
+
+    static propTypes = {
+        searchName: PropTypes.string.isRequired
+    }
 
     state = {
         initView: true,
@@ -9,12 +15,39 @@ export default class Main extends Component{
         errorMsg: null
     }
 
+    // 当组件接收到新的属性时调用
+    componentWillReceiveProps(newProps, nextContext) {
+        const {searchName} = newProps;
+        this.setState({
+            initView: false,
+            loading: true
+        })
+        const url = `https://api.github.com/search/users?q=${searchName}`;
+        axios.get(url)
+            .then(response => {
+                const result = response.data;
+                const users = result.items.map(item => {
+                    return {name: item.login,url: item.html_url, avatarUrl: item.avatar_url}
+                })
+                this.setState({
+                    loading: false,
+                    users
+                })
+            })
+            .catch(error => {
+                this.setState({
+                    loading: false,
+                    errorMsg: error.message
+                })
+            })
+    }
+
     render() {
-        const {initView, loading, users, errorMsg} = this.state
+        const {initView, loading, users, errorMsg} = this.state;
         if(initView) {
             return <h2>Please enter keyword search...</h2>
         }else if(loading){
-            return <h2>In the process of requesting, please wait a moment...</h2>
+            return <h2>Loading Result...</h2>
         }else if(errorMsg) {
             return <h2>{errorMsg}</h2>
         }else {
@@ -22,9 +55,9 @@ export default class Main extends Component{
                 <div className="row">
                     {
                         users.map((user,index) => (
-                            <div className="card col-sm-4 text-center">
+                            <div className="card col-sm-4 text-center" key={index}>
                                 <a href={user.url}>
-                                    <img src={user.avatarUrl} style={{width: 100}}/>
+                                    <img src={user.avatarUrl} style={{width: 100}} alt={user.name}/>
                                 </a>
                                 <p className="card-text">{user.name}</p>
                             </div>
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,14 +1,27 @@
 import React,{Component} from 'react'
+import PropTypes from 'prop-types'
 
 export default class Search extends Component{
 
+    static propTypes = {
+        setSearchName: PropTypes.func.isRequired
+    }
+
+    search = () => {
+        // 得到输入的关键字
+        const serchName = this.searchKey.value.trim();
+        if(serchName){
+            // 搜索
+            this.props.setSearchName(serchName)
+        }
+    }
     render() {
         return (
             <section className="jumbotron">
                 <h3 className="jumbotron-heading">Search Github Users</h3>
                 <div>
-                    <input type="text" className="col-sm-4 mr-1 search_name" placeholder="enter the name you search" />
-                    <button>Search</button>
+                    <input type="text" className="col-sm-4 mr-1 search_name" placeholder="enter the name you search" ref={input => this.searchKey = input} />
+                    <button onClick={this.search}>Search</button>
                 </div>
             </section>
         )
