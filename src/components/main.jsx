3d27966fb640bfd6139836147b8bfbd82544d4dd
baogaijie <1006214269@qq.com>
2019-06-28 10:58:21 +0800

    【路尔轩】定义搜索结果页的四种状态

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,16 +2,36 @@ import React,{Component} from 'react'
 
 export default class Main extends Component{
 
+    state = {
+        initView: true,
+        loading: false,
+        users: null,
+        errorMsg: null
+    }
+
     render() {
-        return (
-            <div className="row">
-                <div className="card col-sm-4 text-center">
-                    <a href="https://github.com/reactjs">
-                        <img src="https://avatars0.githubusercontent.com/u/6412038?s=200&v=4" style={{width: 100}}/>
-                    </a>
-                    <p className="card-text">reactjs</p>
+        const {initView, loading, users, errorMsg} = this.state
+        if(initView) {
+            return <h2>Please enter keyword search...</h2>
+        }else if(loading){
+            return <h2>In the process of requesting, please wait a moment...</h2>
+        }else if(errorMsg) {
+            return <h2>{errorMsg}</h2>
+        }else {
+            return (
+                <div className="row">
+                    {
+                        users.map((user,index) => (
+                            <div className="card col-sm-4 text-center">
+                                <a href={user.url}>
+                                    <img src={user.avatarUrl} style={{width: 100}}/>
+                                </a>
+                                <p className="card-text">{user.name}</p>
+                            </div>
+                        ))
+                    }
                 </div>
-            </div>
-        )
+            )
+        }
     }
 }
